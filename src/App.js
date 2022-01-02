31b8d666d0780e0ea14d27b89860b47abaf16149
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2022-01-02 22:05:30 +0600

    create develop branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React,{Component} from "react";
-import Navbar from "./components/navbar.component";
-import Products from "./components/products.component";
+import React, {Component} from "react";
 
 class App extends Component {
-    state = {
-        totalCart: 0
-    }
-
-    updateCart = (value) => {
-        this.setState({
-            totalCart: value
-        })
-    }
-
     render() {
         return (
             <>
-                <Navbar totalCart={this.state.totalCart}/>
-                <Products updateCart={this.updateCart}/>
+                Hello Facebook
             </>
         );
     }
